2473b873c88a1e3c75068e987b5a96844bf404d6
Pierre Gronnier <pierre.gronnier@gmail.com>
2025-09-15 15:43:05 +0200

    add the message board

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const path = require("node:path");
 const PORT = 3000;
 
+// Middleware pour parser les données du formulaire
+app.use(express.urlencoded({ extended: true }));
+
 // Configuration ejs
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -11,18 +14,46 @@ app.set("view engine", "ejs");
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
 
-// Configuration des routes
-const links = [
-  { href: "/", text: "Home" },
-  { href: "new", text: "New message" },
+// Init message
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
 ];
 
+// Routes
 app.get("/", (req, res) => {
-  res.render("index", { links: links });
+  res.render("index", { messages: messages });
 });
 
 app.get("/new", (req, res) => {
-  res.render("new", { links: links });
+  res.render("new");
+});
+
+app.post("/new", (req, res) => {
+  const { user, text } = req.body;
+  messages.push({
+    text: text,
+    user: user,
+    added: new Date(),
+  });
+  res.redirect("/");
+});
+
+app.get("/message/:id", (req, res) => {
+  const messageId = req.params.id;
+  if (messages[messageId]) {
+    res.render("message", { message: messages[messageId] });
+  } else {
+    res.status(404).send("Message not found");
+  }
 });
 
 // Démarrage du serveur
